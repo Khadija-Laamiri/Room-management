1403d5f1ca4cd598d1768abb79d7133441c718ed
huda-do-5 <elhirmhouda@gmail.com>
2023-12-12 23:50:56 +0100

    leaved students fix 3

--- src/room_management/LeavedStudents.java
+++ src/room_management/LeavedStudents.java
@@ -3,8 +3,6 @@ package room_management;
 import java.sql.*;
 import Project.DBconnection;
 import javax.swing.JOptionPane;
-import javax.swing.JTable;
-import javax.swing.SwingUtilities;
 import javax.swing.table.DefaultTableModel;
 
 /*
@@ -22,33 +20,24 @@ public class LeavedStudents extends javax.swing.JFrame {
      */
     public LeavedStudents() {
         initComponents();
-        DefaultTableModel model = (DefaultTableModel) jTable1.getModel();
-        jTable1.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
-        try {
-            Connection con = DBconnection.getCon();
-            Statement st = con.createStatement();
-            ResultSet rs = st.executeQuery("select * from student where status='leaved'");
-            while (rs.next()) {
-                model.addRow(new Object[]{rs.getString(2), rs.getString(1), rs.getString(3), rs.getString(4), rs.getString(5), rs.getString(6), rs.getString(7), rs.getString(8), rs.getString(9)});
         
-        SwingUtilities.invokeLater(()-> {
-            DefaultTableModel model = (DefaultTableModel) jTable1.getModel();
-            jTable1.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
-            
-            try {
-                Connection con = DBconnection.getCon();
-                Statement st = con.createStatement();
-                ResultSet rs = st.executeQuery("select * from student where status = 'leaved' ");
-                
-                while (rs.next()) {
-                    model.addRow(new Object[]{rs.getString(2), rs.getString(1), rs.getString(3), rs.getString(4), rs.getString(5), rs.getString(6), rs.getString(7), rs.getString(8), rs.getString(9)});
-                }
-            } catch (SQLException e) {
-                e.printStackTrace();
-                JOptionPane.showMessageDialog(null, e);
+        DefaultTableModel model=(DefaultTableModel) jTable1.getModel();
+        //jTable1.setAutoResizeMode(jTable1.AUTO_RESIZE_OFF);
+        try{
+        
+            Connection con =DBconnection.getCon();
+            Statement st=con.createStatement();
+            ResultSet rs=st.executeQuery("select *from student where status='leaved'");
+            while(rs.next()){
+               model.addRow(new Object[]{rs.getString(2),rs.getString(1),rs.getString(3),rs.getString(4),rs.getString(5),rs.getString(6),rs.getString(7),rs.getString(8),rs.getString(9 )});
             }
-        });
+        }
+        catch(Exception e){
+            JOptionPane.showMessageDialog(null, e);      
+        }
+        
     }
+     
 
     /**
      * This method is called from within the constructor to initialize the form.
@@ -68,8 +57,6 @@ public class LeavedStudents extends javax.swing.JFrame {
         setLocation(new java.awt.Point(480, 150));
         setUndecorated(true);
 
-        jTable1.setBackground(new java.awt.Color(153, 204, 255));
-        jTable1.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
         jTable1.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
 
@@ -92,30 +79,17 @@ public class LeavedStudents extends javax.swing.JFrame {
         }
 
         jPanel1.setBackground(new java.awt.Color(255, 255, 255));
+        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         jButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/icons8-close-30.png"))); // NOI18N
         jButton1.setBorder(null);
-        jButton1.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        jButton1.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
         jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton1ActionPerformed(evt);
             }
         });
-
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addGap(0, 0, Short.MAX_VALUE)
-                .addComponent(jButton1))
-        );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addComponent(jButton1)
-                .addGap(0, 470, Short.MAX_VALUE))
-        );
+        jPanel1.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(770, 0, -1, -1));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -123,16 +97,17 @@ public class LeavedStudents extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
             .addGroup(layout.createSequentialGroup()
-                .addGap(10, 10, 10)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 784, Short.MAX_VALUE)
+                .addContainerGap()
+                .addComponent(jScrollPane1)
                 .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(57, 57, 57)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 430, javax.swing.GroupLayout.PREFERRED_SIZE))
-            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 430, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
         );
 
         pack();
@@ -177,6 +152,8 @@ public class LeavedStudents extends javax.swing.JFrame {
             }
         });
     }
+
+    
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
